fe61f2180482e9199212f93f277b1489326f7303
yaakov-avraham <yaakova@wix.com>
2021-11-24 23:54:26 +0200

    get productId from event and use dynamic settings config

--- public/webComponent.js
+++ public/webComponent.js
@@ -1,40 +1,67 @@
-// class MyComponent extends HTMLElement {
-//   constructor() {
-//     // Always call super first in constructor
-//     super();
-//   }
-//   connectedCallback() {
-//     setTimeout(() => {
-//       console.log("before");
-//       this.dispatchEvent(new CustomEvent('web-component-event'));
-//       console.log("after");
-//     }, 500)
-//     debugger;
-//     this.dispatchEvent(new CustomEvent('web-component-event'));
-//     const wixconfig = JSON.parse(this?.attributes?.wixconfig?.value || '{}');
-//     // current instanceId
-//     const instanceId = wixconfig?.instanceId || '';
-//     console.log("sapir")
-//     console.log({wixconfig})
-//     this.setAttribute('style', 'display:block')
-//     this.innerHTML = `
-//       <div style="display: flex;
-//           align-items: center;
-//           justify-content: center;
-//           flex-direction: column;
-//           width: 100%;
-//           height: 100vh;
-//           background: #f0f8ff;
-//         ">
-//           <h1>This is a Web Component demo sapir</h1>
-//       </div>
-//     `;
-//   }
-// }
+const appId = '42683fdd-cbe6-43a7-a75d-92b70d37e7cd';
+let idOfViewedProduct;
 
-// customElements.define('my-component', MyComponent);
+class MyComponent extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+  }
+  connectedCallback() {
+    const wixconfig = JSON.parse(this?.attributes?.wixconfig?.value || '{}');
+    console.log({ wixconfig });
+    const {
+      title = 'no value',
+      button = 'no button text',
+      backgroundColor,
+      borderColor,
+      borderWidth = 0,
+    } = wixconfig?.data || {};
+    this.setAttribute('style', 'display:block');
+    this.innerHTML = `
+      <div style="display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-direction: column;
+          width: 100%;
+          padding: 10px 0;
+          background-color: ${
+            backgroundColor?.color
+              ? this.rgbaColorToRgbColor(backgroundColor.color)
+              : '#fff'
+          };
+          border: ${borderWidth}px solid ${
+      borderColor?.color ? this.rgbaColorToRgbColor(borderColor.color) : '#fff'
+    }
+        ">
+          <h2 style="margin-bottom: 10px;">${title}</h2>
+          <div style="display: flex;width: 55%;justify-content: space-between;">
+              <input id="email-input" placeholder="your email" type="text" id="email" name="email">
+              <button id="send-button" style="border: 1px solid black; padding: 5px;" type="button" >${button}</button>
+          </div>
+      </div>
+    `;
+    this.runScript();
+    document
+      .querySelector('#send-button')
+      .addEventListener('click', this.onButtonClick);
+  }
+  runScript() {
+    window.wixDevelopersAnalytics?.register?.(appId, (eventName, data) => {
+      switch (eventName) {
+        case 'productPageLoaded':
+          const { productId } = data;
+          idOfViewedProduct = productId;
+          break;
+      }
+    });
+  }
+  onButtonClick() {
+    const visitorEmail = document.querySelector('#email-input').value;
+    console.log({ visitorEmail, idOfViewedProduct });
+  }
+  rgbaColorToRgbColor(rgbaColor) {
+    return rgbaColor.replace('a', '').split(',').slice(0, 3).join(',') + ')';
+  }
+}
 
-(function(){
-  console.log("blabla");
-  alert("bla");
-})()
+customElements.define('my-own-component', MyComponent);
